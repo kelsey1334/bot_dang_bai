2c2b0daa2d299a83e2f3d2ee4c2d064361e2d19b
kelsey1334 <kelsey@procons.one>
2025-06-16 23:02:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -122,6 +122,10 @@ def post_to_wordpress(keyword, article_data):
         {'key': 'rank_math_title', 'value': article_data["meta_title"]},
         {'key': 'rank_math_description', 'value': article_data["meta_description"]},
         {'key': 'rank_math_focus_keyword', 'value': article_data["focus_keyword"]},
+        {'key': 'rank_math_keywords', 'value': article_data["focus_keyword"]}
+    ]},
+        {'key': 'rank_math_description', 'value': article_data["meta_description"]},
+        {'key': 'rank_math_focus_keyword', 'value': article_data["focus_keyword"]},
         {'key': 'rank_math_keywords', 'value': article_data["focus_keyword"]},
     ]},
         {'key': 'rank_math_description', 'value': article_data["meta_description"]},
