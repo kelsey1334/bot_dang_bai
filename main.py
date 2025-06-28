3439bb9ea98a29c62482a3f2db51baba093e74ae
kelsey1334 <kelsey@procons.one>
2025-06-28 23:32:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ Yêu cầu cụ thể như sau:
 - Thêm 3 ba từ khoá tự phụ ngữ nghĩa để bổ trợ
 - In đậm từ khóa chính.
 ---
-⚠️ Lưu ý: Viết bằng tiếng Việt, giọng văn rõ ràng, dễ hiểu, không lan man. Ưu tiên thông tin hữu ích, ví dụ thực tế, và có chiều sâu để tăng điểm chuyên môn với Google. Ngoài ra, các tiêu đề không được làm dạng bullet chỉ cần có định dạng tiêu đề là được rồi. Không cần phải có những thông tin lưu ý và câu hỏi mở rộng gì, thứ tôi cần chỉ là một bài content chuẩn seo, còn thêm một lưu ý nữa là bạn không cần phải khoe là bạn làm gì với bài viết này vào cuối câu trả lời khi viết xong bài viết này thứ tôi cần chỉ là bài viết'''
+⚠️ Lưu ý: Viết bằng tiếng Việt, giọng văn rõ ràng, dễ hiểu, không lan man. Ưu tiên thông tin hữu ích, ví dụ thực tế, và có chiều sâu để tăng điểm chuyên môn với Google. Ngoài ra, các tiêu đề không được làm dạng bullet chỉ cần có định dạng tiêu đề là được rồi. Không cần phải có những thông tin lưu ý và câu hỏi mở rộng gì, thứ tôi cần chỉ là một bài content chuẩn seo'''
 
 logging.basicConfig(level=logging.INFO)
 openai_client = AsyncOpenAI(api_key=OPENAI_API_KEY)
